37c324f8dc938c5c62b1d60d6662cbf9c60fc9ac
Taylor Blocker <taylorblocker@gmail.com>
2019-08-19 20:58:57 -0500

    Spoke with professor and found a good way to get what i needed interacting correctly. I was close yesterday when moving my movieClickEvent function outside the domcontent function, but still needed to target the specific items and find a way to input the correct IMDB datab. Using the data= method to assign data to specific elements

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ document.addEventListener('DOMContentLoaded',function()
             
             return `
             <div class="movie card" style="">
-            <img class="card-img-top movie_poster" src="${currentMovie.Poster}" alt="${currentMovie.Title}">
-            <div class="card-body">
-            <div class="movie_text">
-            <div class="card-text movie_title">${currentMovie.Title}</div>
-            <div class="card-text release_date">${currentMovie.Year}</div>
-            </div>
-            <button data-movieid="${currentMovie.imdbID}" id="add_button" class="btn btn-primary">Add</b>
-            </div>
+                <img class="card-img-top movie_poster" src="${currentMovie.Poster}" alt="${currentMovie.Title}">
+                <div class="card-body">
+                    <div class="movie_text">
+                        <div class="card-text movie_title">${currentMovie.Title}</div>
+                        <div class="card-text release_date">${currentMovie.Year}</div>
+                    </div>
+                    <button data-movieid="${currentMovie.imdbID}" id="add_button" class="btn btn-primary">Add</b>
+                </div>
             </div>
             `
         });
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded',function()
     }
 
     let moviesContainer = getById('movies_container');
-    let movieSearchID = getById.('add_button');
+    // let movieSearchID = getById.('add_button');
    
     function getById(id){
         return document.getElementById(id)
@@ -37,20 +37,27 @@ document.addEventListener('DOMContentLoaded',function()
         moviesContainer.innerHTML = finalHTML
     });
     
+    
+});
+    
+    // document.getElementById('add_button').addEventListener('click',saveToWatchlist(`${movieData.imdbID}`));
+    
+    // function saveToWatchlist(imdbID){
+        // console.log(imdbID.imdbID)    
+        // }
+        
+    
     function saveToWatchlist(imdbID){
         // imdbID.preventDefault();
         console.log("You saved a movie! ID" + imdbID)
-    
+
+
     };
 
-
-    // document.getElementById('add_button').addEventListener('click',saveToWatchlist(`${movieData.imdbID}`));
-
-    // function saveToWatchlist(imdbID){
-    // console.log(imdbID.imdbID)    
-    // }
-
-});
-
-
+    function movieClickEvent(event){
+        let targetEl = event.target
+        let movieID = event.target.getAttribute("data-movieid")
+        if(targetEl.id === "add_button")
+        saveToWatchlist(movieID)
+    }
 
